b822a7eeb42f0bf102c98a4dd14973e974b7acd2
Matías Ignacio González Toledo <ignacio.gonzalez.t1963@gmail.com>
2026-06-12 01:53:22 -0400

    campeones LoL

---
+++ Campeones.py
@@ -0,0 +1,31 @@
+campeones = {
+    "Garen":{"roles":["TOP"],"winrate":51},
+    "Darius":{"roles":["TOP"],"winrate":52},
+    "Sett":{"roles":["TOP","JG"],"winrate":50},
+    "Malphite":{"roles":["TOP"],"winrate":53},
+    "Aatrox":{"roles":["TOP"],"winrate":49},
+
+    "Lee Sin":{"roles":["JG"],"winrate":49},
+    "Vi":{"roles":["JG"],"winrate":52},
+    "Jarvan IV":{"roles":["JG"],"winrate":51},
+    "Viego":{"roles":["JG"],"winrate":50},
+    "Wukong":{"roles":["TOP","JG"],"winrate":52},
+
+    "Ahri":{"roles":["MID"],"winrate":52},
+    "Yasuo":{"roles":["MID","TOP"],"winrate":49},
+    "Zed":{"roles":["MID"],"winrate":50},
+    "Veigar":{"roles":["MID"],"winrate":53},
+    "Lux":{"roles":["MID","BOTTOM"],"winrate":51},
+
+    "Jinx":{"roles":["BOTTOM"],"winrate":52},
+    "Vayne":{"roles":["TOP","BOTTOM"],"winrate":56},
+    "Ashe":{"roles":["BOTTOM"],"winrate":51},
+    "Caitlyn":{"roles":["BOTTOM"],"winrate":53},
+    "Miss Fortune":{"roles":["BOTTOM"],"winrate":52},
+
+    "Thresh":{"roles":["BOTTOM"],"winrate":51},
+    "Nautilus":{"roles":["BOTTOM"],"winrate":52},
+    "Leona":{"roles":["BOTTOM"],"winrate":50},
+    "Morgana":{"roles":["MID","BOTTOM"],"winrate":51},
+    "Pyke":{"roles":["MID","BOTTOM"],"winrate":53}
+}
